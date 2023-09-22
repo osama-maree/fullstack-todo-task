78e1f3cfd62932fc241853501721b4350b86120b
osama maree <osamamarei2001@gmail.com>
2023-09-22 14:12:04 +0300

    Setup Server And Database

--- server-side/app.js
+++ server-side/app.js
@@ -1,12 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-
 const app = express();
 dotenv.config({ path: "./config/.env" });
 app.use(express.json());
-
 const port = process.env.PORT || 3000;
-
 app.use("*", (req, res) => {
   res.status(404).json({ message: "page is not found" });
 });
