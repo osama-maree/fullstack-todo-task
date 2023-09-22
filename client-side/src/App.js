b96e417324c78594b78dfe1798629e2fd59e2f52
osama maree <osamamarei2001@gmail.com>
2023-09-22 11:00:19 +0300

    AddA WworkingMethod

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,9 +1,17 @@
-
+import { Container } from "@mui/material";
+import Home from "./pages/Home.jsx";
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <Container
+      maxWidth="sm"
+      sx={{
+        border: "1px solid #eee",
+        borderRadius: "15px",
+        paddingTop: "10px",
+      }}
+    >
+      <Home />
+    </Container>
   );
 }
 
